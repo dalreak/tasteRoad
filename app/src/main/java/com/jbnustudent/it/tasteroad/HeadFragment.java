157d0572b0822f54db5f0eae9462d0317abb0093
dalreak <toojs@naver.com>
2019-01-09 23:24:58 +0900

    update fragment
    test

---
+++ app/src/main/java/com/jbnustudent/it/tasteroad/HeadFragment.java
@@ -0,0 +1,19 @@
+package com.jbnustudent.it.tasteroad;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HeadFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.head_view, container, false);
+    }
+
+}
+
+
